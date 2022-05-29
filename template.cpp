c1b7e0c0e057b53455ffd615d14c60582fe4d177
kalhemis <106173460+kalhemis@users.noreply.github.com>
2022-05-29 15:09:57 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 #define i64             long long
+#define pii             pair<int, int> 
 #define pb              push_back
 #define eb              emplace_back
 #define endl            "\n"
 #define sz(a)           (int) a.size()
 
-bool MULTI_TEST = 1;
+bool multi_test = 1;
 
 void solve() {
 
@@ -21,8 +22,8 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false); 
     cin.tie(0);
-    int TESTS = 1;
-    if (MULTI_TEST) cin >> TESTS;
-    while (TESTS--) solve();
+    int tests = 1;
+    if (multi_test) cin >> tests;
+    while (tests--) solve();
     return 0;
 }
